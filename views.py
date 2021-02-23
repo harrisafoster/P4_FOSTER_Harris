4dfed172d3c613d05b0e81e8a2faf050705e0d3d
harrisafoster <harrisafoster@gmail.com>
2021-02-23 14:45:19 +0100

    Ajout de nouvelles fonctions show_all_tournament_names(tournaments), def show_one_tournament(tournament_object, name_of_tournament)

--- views.py
+++ views.py
@@ -1,9 +1,17 @@
-
-
 def show_players(players):
+    print('Currently, the saved players in the database are: ')
     for player in players:
         print(player)
 
 
 def show_one_player(player_object, email):
     print(player_object.read_one_player(email))
+
+
+def show_all_tournament_names(tournaments):
+    for tournament in tournaments:
+        print(tournament)
+
+
+def show_one_tournament(tournament_object, name_of_tournament):
+    print(tournament_object.read_one_tournament(name_of_tournament))
