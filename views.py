35318ea15eaf95a4d8347af9a92cef5edb0801c2
harrisafoster <harrisafoster@gmail.com>
2021-02-23 11:17:09 +0100

    Intégration des objets préferant la contruction self de la classe Player.

--- views.py
+++ views.py
@@ -2,9 +2,11 @@ from player import Player
 
 
 def show_players():
-    for player in Player.read_player_list():
+    player_object = Player('players.json')
+    for player in player_object.read_player_list():
         print(player)
 
 
 def show_one_player(email):
-    print(Player.read_one_player(email))
+    player_object = Player('players.json')
+    print(player_object.read_one_player(email))
