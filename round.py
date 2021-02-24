465e94460e30c5b5fade82075827799306845c5a
harrisafoster <harrisafoster@gmail.com>
2021-02-24 19:14:17 +0100

    Simplification des méthodes init, create_round, read_one_round, read_all_rounds, et delete_round.

--- round.py
+++ round.py
@@ -3,16 +3,16 @@ import datetime
 
 
 class Round:
-    query = Query()
-
-    def __init__(self, tournament_name, round_number, date, start_of_round, end_of_round, matches, time_ctrl):
-        self.tournament_name = tournament_name
-        self.round_number = round_number
-        self.date = date
-        self.start_of_round = start_of_round
-        self.end_of_round = end_of_round
-        self.matches = matches
-        self.time_ctrl = time_ctrl
+    def __init__(self, json_file):
+        self.db_tournaments = TinyDB(json_file)
+        self.query = Query()
+        self.tournament_name = ""
+        self.round_number = ""
+        self.date = ""
+        self.start_of_round = None
+        self.end_of_round = ""
+        self.matches = []
+        self.time_ctrl = ""
 
     def serialize_round(self):
         round_data = {
@@ -26,43 +26,24 @@ class Round:
         }
         return round_data
 
-    # TODO improve?
-    @classmethod
-    def create_round(cls, round_number, tournament):
-        tournament_name = tournament.name
-        date = str(datetime.date.today())
-        start_of_round = str(datetime.datetime.now().strftime("%H:%M:%S"))
-        end_of_round = None
-        matches = tournament.round_descriptions[round_number - 1]
-        time_ctrl = tournament.time_ctrl
-        round_instance_data = Round(tournament_name, round_number,
-                                    date, start_of_round, end_of_round, matches, time_ctrl)
-        return round_instance_data
+    def create_round(self, round_number, tournament):
+        self.tournament_name = tournament.name_of_tournament
+        self.round_number = round_number
+        self.date = str(datetime.date.today())
+        self.start_of_round = datetime.datetime.now()
+        self.end_of_round = ""
+        self.matches = tournament.round_descriptions[round_number - 1]
+        self.time_ctrl = tournament.time_ctrl
+        return self
 
-    # TODO test
-    @classmethod
-    def read_one_round(cls, round_number, tournament):
-        db_tournaments = TinyDB('tournaments.json')
-        current_tournament = db_tournaments.search(Round.query.tournament_name == tournament.name)
+    def read_one_round(self, round_number, tournament):
+        current_tournament = self.db_tournaments.search(self.query.name_of_tournament == tournament.name_of_tournament)
         return current_tournament['round_instances'][round_number - 1]
 
-    # TODO test
-    @classmethod
-    def read_all_rounds(cls, tournament):
-        db_tournaments = TinyDB('tournaments.json')
-        current_tournament = db_tournaments.search(Round.query.tournament_name == tournament.name)
+    def read_all_rounds(self, tournament):
+        current_tournament = self.db_tournaments.search(self.query.name_of_tournament == tournament.name_of_tournament)
         return current_tournament['round_instances']
 
-    # TODO test
-    @classmethod
-    def update_round(cls, round_number, tournament):
-        db_tournaments = TinyDB('tournaments.json')
-        db_tournaments.update({'round_instances'[round_number - 1]: tournament.round_instances[round_number - 1]},
-                              Round.query["Name of tournament: "] == tournament.name)
-
-    # TODO test
-    @classmethod
-    def delete_round(cls, round_number, tournament):
-        db_tournaments = TinyDB('tournaments.json')
-        db_tournaments.remove(
+    def delete_round(self, round_number, tournament):
+        self.db_tournaments.remove(
             where('tournament_name') == tournament.name and where('round_number') == round_number)
