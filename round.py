0decb74b5f1711fd4f4f17c411ac7f25beb5651a
harrisafoster <harrisafoster@gmail.com>
2021-02-25 17:40:26 +0100

    Simplification de la classe Round Simplification des méthodes de lecture Suppression des méthods cls Déplacement des méthodes update et delete dans la classe Tournament.

--- round.py
+++ round.py
@@ -1,4 +1,4 @@
-from tinydb import TinyDB, Query, where
+from tinydb import TinyDB, Query
 import datetime
 
 
@@ -7,7 +7,7 @@ class Round:
         self.db_tournaments = TinyDB(json_file)
         self.query = Query()
         self.tournament_name = ""
-        self.round_number = ""
+        self.round_number = 0
         self.date = ""
         self.start_of_round = None
         self.end_of_round = ""
@@ -38,12 +38,8 @@ class Round:
 
     def read_one_round(self, round_number, tournament):
         current_tournament = self.db_tournaments.search(self.query.name_of_tournament == tournament.name_of_tournament)
-        return current_tournament['round_instances'][round_number - 1]
+        return current_tournament[0]['round_instances'][round_number - 1]
 
     def read_all_rounds(self, tournament):
         current_tournament = self.db_tournaments.search(self.query.name_of_tournament == tournament.name_of_tournament)
-        return current_tournament['round_instances']
-
-    def delete_round(self, round_number, tournament):
-        self.db_tournaments.remove(
-            where('tournament_name') == tournament.name and where('round_number') == round_number)
+        return current_tournament[0]['round_instances']
