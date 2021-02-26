d6d62bdf4fae62497a85c089e91926479f3f5b72
harrisafoster <harrisafoster@gmail.com>
2021-02-26 12:16:05 +0100

    Ajout de toutes les fonctions nécéssaires pour que l'utilisateur puisse générer des rapports locaux et globaux.

--- controllers.py
+++ controllers.py
@@ -52,17 +52,8 @@ def update_player_data():
             else:
                 views.show_one_player(Player('players.json'), email)
                 break
-        # TODO big prints in views
-        fields_to_edit = input("Which fields would you like to edit?"
-                               "\n (1) last_name "
-                               "\n (2) first_name "
-                               "\n (3) date_of_birth "
-                               "\n (4) sex "
-                               "\n (5) ranking"
-                               "\n (6) email"
-                               "\n Enter only the numerical values of the fields "
-                               "that you would like to edit. (ex. 1 2 3)"
-                               "\n Input:  ")
+        views.show_modifiable_fields()
+        fields_to_edit = input()
         if '1' in fields_to_edit:
             last_name = input("Enter the player's modified last name: ").upper()
         else:
@@ -130,8 +121,7 @@ def remove_player():
 
 
 def get_new_tournament_data():
-    name_of_tournament = str(input('Enter name of tournament: ')) + ' ' + str(
-        datetime.date.today().strftime("%d/%m/%y"))
+    name_of_tournament = str(input('Enter name of tournament: ') + ' ' + str(datetime.date.today()))
     location = input('Enter location of tournament: ')
     while True:
         try:
@@ -188,7 +178,8 @@ def get_new_tournament_data():
         else:
             break
     description = input('Enter the description of the tournament: ')
-    Tournament('tournaments.json').create_tournament(name_of_tournament, location, nb_rounds, player_emails, time_ctrl, description)
+    Tournament('tournaments.json').create_tournament(name_of_tournament, location, nb_rounds,
+                                                     player_emails, time_ctrl, description)
     current_tournament = Tournament('tournaments.json').access_tournament_object(name_of_tournament)
     current_tournament.populate_player_instances()
     return current_tournament.name_of_tournament
@@ -309,6 +300,385 @@ def point_counter(current_tournament, round_number):
     current_tournament.update_player_instances(current_tournament.player_instances)
 
 
+def local_player_report_by_last_name(chosen_tournament):
+    views.show_list(chosen_tournament.sort_players_by_last_name())
+    views.when_finished()
+    all_done = input()
+    if all_done == 'q':
+        main_menu()
+    else:
+        report_menu_local_reports()
+
+
+def local_player_report_by_relative_ranking(chosen_tournament):
+    views.show_list(chosen_tournament.sort_players_by_local_index())
+    views.when_finished()
+    all_done = input()
+    if all_done == 'q':
+        main_menu()
+    else:
+        report_menu_local_reports()
+
+
+def local_player_report_by_points(chosen_tournament):
+    views.show_list(chosen_tournament.sort_players_by_points_descending())
+    views.when_finished()
+    all_done = input()
+    if all_done == 'q':
+        main_menu()
+    else:
+        report_menu_local_reports()
+
+
+def local_round_report(chosen_tournament):
+    views.show_list(Round('tournaments.json').read_all_rounds(chosen_tournament))
+    views.when_finished()
+    all_done = input()
+    if all_done == 'q':
+        main_menu()
+    else:
+        report_menu_local_reports()
+
+
+def local_match_report(chosen_tournament):
+    views.show_list(Match('tournaments.json').read_all_matches(chosen_tournament))
+    views.when_finished()
+    all_done = input()
+    if all_done == 'q':
+        main_menu()
+    else:
+        report_menu_local_reports()
+
+
+def report_menu_local_reports():
+    views.show_all_tournament_names_and_dates(Tournament('tournaments.json').read_all_tournaments())
+    if len(Tournament('tournaments.json').read_all_tournaments()) == 0:
+        print("There are no currently saved tournaments.")
+        views.when_finished()
+        all_done = input()
+        if all_done:
+            main_menu()
+    else:
+        while True:
+            choice = input('On which tournament would you like a report? (name and date only, format: Name yyyy-mm-dd)')
+            try:
+                assert choice in Tournament('tournaments.json').get_tournament_names()
+            except AssertionError:
+                print("Sorry, you need to choose one of the available options.")
+                continue
+            else:
+                break
+        chosen_tournament = Tournament('tournaments.json').access_tournament_object(choice)
+        while True:
+            views.show_local_tournament_report_options()
+            sub_choice = input()
+            try:
+                int(sub_choice)
+            except ValueError:
+                print("Sorry, I didn't understand that.")
+                continue
+
+            if int(sub_choice) not in range(8) or int(sub_choice) == 0:
+                print("Sorry, you need to select one of the available options.")
+                continue
+            else:
+                sub_choice = int(sub_choice)
+                break
+    if sub_choice == 1:
+        local_player_report_by_last_name(chosen_tournament)
+    if sub_choice == 2:
+        local_player_report_by_relative_ranking(chosen_tournament)
+    if sub_choice == 3:
+        local_player_report_by_points(chosen_tournament)
+    if sub_choice == 4:
+        local_round_report(chosen_tournament)
+    if sub_choice == 5:
+        local_match_report(chosen_tournament)
+    if sub_choice == 6:
+        main_menu()
+    if sub_choice == 7:
+        main_report_menu()
+
+
+def global_report_players_by_last_name():
+    views.show_list(Player('players.json').sort_all_players_by_last_name())
+    views.when_finished()
+    all_done = input()
+    if all_done == 'q':
+        main_menu()
+    else:
+        global_players_report_options()
+
+
+def global_report_players_by_ranking():
+    views.show_list(Player('players.json').sort_all_players_by_ranking())
+    views.when_finished()
+    all_done = input()
+    if all_done == 'q':
+        main_menu()
+    else:
+        global_players_report_options()
+
+
+def global_players_report_options():
+    while True:
+        views.show_global_players_report_options()
+        choice = input()
+        try:
+            int(choice)
+        except ValueError:
+            print("Sorry, I didn't understand that.")
+        if int(choice) not in range(5) or int(choice) == 0:
+            print("Sorry, you need to select one of the available options.")
+            continue
+        else:
+            choice = int(choice)
+            break
+    if choice == 1:
+        global_report_players_by_last_name()
+    if choice == 2:
+        global_report_players_by_ranking()
+    if choice == 3:
+        main_menu()
+    if choice == 4:
+        report_menu_global_reports()
+
+
+def report_all_tournaments():
+    for tournament in Tournament('tournaments.json').db_tournaments.all():
+        views.show_tournament(tournament)
+    views.when_finished()
+    all_done = input()
+    if all_done == 'q':
+        main_menu()
+    else:
+        report_menu_global_reports()
+
+
+def report_menu_global_reports():
+    while True:
+        views.show_global_report_options()
+        choice = input()
+        try:
+            int(choice)
+        except ValueError:
+            print("Sorry, I didn't understand that.")
+        if int(choice) not in range(5) or int(choice) == 0:
+            print("Sorry, you need to select one of the available options.")
+            continue
+        else:
+            choice = int(choice)
+            break
+    if choice == 1:
+        global_players_report_options()
+    if choice == 2:
+        report_all_tournaments()
+    if choice == 3:
+        main_menu()
+    if choice == 4:
+        main_report_menu()
+
+
+def main_report_menu():
+    while True:
+        views.show_main_report_menu_choices()
+        choice = input()
+        try:
+            int(choice)
+        except ValueError:
+            print("Sorry, I didn't understand that.")
+
+        if int(choice) not in range(4) or int(choice) == 0:
+            print("Sorry, you need to select one of the available options.")
+            continue
+        else:
+            choice = int(choice)
+            break
+    if choice == 1:
+        report_menu_global_reports()
+    if choice == 2:
+        report_menu_local_reports()
+    if choice == 3:
+        main_menu()
+
+
+'''def menu_2_4():
+    players_to_add = input('Would you like to add any players to the database? (y/n)')
+    if players_to_add == 'y':
+        number_to_add = input('How many players would you like to add? (numbers only)')
+        for x in range(int(number_to_add)):
+            Player.add_player(None)
+        all_done = input(
+            "When you're finished, press q to return to the main menu or press b to return to the previous menu.")
+        if all_done == 'q':
+            main_menu()
+        else:
+            menu_2()
+    if players_to_add == 'n' or not players_to_add:
+        main_menu()
+
+
+def menu_2_3():
+    Player.edit_player()
+    all_done = input(
+        "When you're finished, press q to return to the main menu or press b to return to the previous menu.")
+    if all_done == 'q':
+        main_menu()
+    else:
+        menu_2()
+
+
+def menu_2_2():
+    Player.remove_player()
+    all_done = input(
+        "When you're finished, press q to return to the main menu or press b to return to the previous menu.")
+    if all_done == 'q':
+        main_menu()
+    else:
+        menu_2()
+
+
+def menu_2_1():
+    query = Query()
+    for item in Player.db_players:
+        print(item)
+        new_ranking = input("What is the player's new ranking? (numbers only)")
+        Player.db_players.update({'Ranking': new_ranking}, query["Player ID"] == item['Player ID'])
+        players = Player.db_players.all()
+        players.sort(key=operator.itemgetter('Ranking'))
+        print('New list of all players sorted by rank: ')
+        for player in players:
+            print(player)
+        all_done = input(
+            "When you're finished, press q to return to the main menu or press b to return to the previous menu.")
+        if all_done == 'q':
+            main_menu()
+        else:
+            menu_2()
+
+
+def menu_2():
+    print("Currently, the players saved in the database are: ")
+    for item in Player.db_players.all():
+        print(item)
+    while True:
+        try:
+            sub_choice = int(input("Would you like to: "
+                                   "\n (1) Edit player rankings"
+                                   "\n (2) Remove a specific player"
+                                   "\n (3) Edit a specific player"
+                                   "\n (4) Add a player or players to the database"
+                                   "\n (5) Return to the main menu"
+                                   "\n Input(number of choice only): "))
+        except ValueError:
+            print("Sorry, I didn't understand that.")
+
+        if sub_choice not in range(6) or sub_choice == 0:
+            print("Sorry, you need to select one of the available options.")
+            continue
+        else:
+            break
+    if sub_choice == 1:
+        menu_2_1()
+    if sub_choice == 2:
+        menu_2_2()
+    if sub_choice == 3:
+        menu_2_3()
+    if sub_choice == 4:
+        menu_2_4()
+    if sub_choice == 5:
+        main_menu()
+
+
+def menu_3_2():
+    proceed = input('Proceed with resuming a tournament? (y/n): ')
+    if proceed == 'y':
+        tournament_names = []
+        for item in Tournament.all_tournaments:
+            tournament_names.append(item['Name of tournament: '])
+        if len(tournament_names) == 0:
+            print("There are no currently saved tournaments.")
+            all_done = input(
+                "When you're finished, press q to return to the main menu or press b to return to the previous menu.")
+            if all_done == 'q':
+                main_menu()
+            else:
+                menu_3()
+        else:
+            resume_tournament()
+            all_done = input(
+                "When you're finished, press q to return to the main menu or press b to return to the previous menu.")
+            if all_done == 'q':
+                main_menu()
+            else:
+                menu_3()
+    if proceed != 'y':
+        menu_3()
+
+
+def menu_3_1():
+    proceed = input('Proceed with creating and starting a tournament? (y/n): ')
+    if proceed == 'y':
+        create_and_start_tournament()
+        all_done = input(
+            "When you're finished, press q to return to the main menu or press b to return to the previous menu.")
+        if all_done == 'q':
+            main_menu()
+        else:
+            menu_3()
+    if proceed != 'y':
+        menu_3()
+
+
+def menu_3():
+    while True:
+        try:
+            sub_choice = int(input("Would you like to: "
+                                   "\n (1) Create and start a tournament"
+                                   "\n (2) Resume a tournament that has already begun"
+                                   "\n (3) Return to the main menu"
+                                   "\n Input(number of choice only): "))
+        except ValueError:
+            print("Sorry, I didn't understand that.")
+
+        if sub_choice not in range(4) or sub_choice == 0:
+            print("Sorry, you need to select one of the available options.")
+            continue
+        else:
+            break
+    if sub_choice == 1:
+        menu_3_1()
+    if sub_choice == 2:
+        menu_3_2()
+    if sub_choice == 3:
+        main_menu()'''
+
+
+def main_menu():
+    while True:
+        views.show_main_menu_choices()
+        choice = input()
+        try:
+            int(choice)
+        except ValueError:
+            print("Sorry, I didn't understand that.")
+
+        if int(choice) not in range(5) or int(choice) == 0:
+            print("Sorry, you need to select one of the available options.")
+            continue
+        else:
+            choice = int(choice)
+            break
+    if choice == 1:
+        main_report_menu()
+    '''if choice == 2:
+        menu_2()
+    if choice == 3:
+        menu_3()
+    if choice == 4:
+        exit(0)'''
+
+
 # TODO Testes ci-dessous, algo fonctionne
 
 '''current_tournament = Tournament('tournaments.json').access_tournament_object(get_new_tournament_data())
@@ -325,3 +695,5 @@ print('End of round 3. ')
 current_tournament.swiss_method_pairing()
 point_counter(current_tournament, 4)
 print('End of round 4. ')'''
+
+main_menu()
